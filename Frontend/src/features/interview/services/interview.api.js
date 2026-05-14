f35f346c5420c17982ac4bfb98db4f3ce0e59e0d
Priyam Aneja <priyamaneja2002@gmail.com>
2026-05-14 16:05:56 +0530

    updated api base url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://interviewai-ro4h.onrender.com",
     withCredentials: true,
 })
 
